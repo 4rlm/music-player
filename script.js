829a53fb6821937935f9b010c4b5f9b82d619613
4rlm <4rlm@protonmail.ch>
2022-08-21 15:02:26 +0900

    add ability to click progress bar to move song to given portion

--- script.js
+++ script.js
@@ -98,12 +98,10 @@ function updateProgressBar(e) {
     progress.style.width = `${progressPercent}%`;
     // Calculate Display for Duration
     const durationMinutes = Math.floor(duration / 60);
-    console.log('minutes', durationMinutes);
     let durationSeconds = Math.floor(duration % 60);
     if (durationSeconds < 10) {
       durationSeconds = `0${durationSeconds}`;
     }
-    console.log('seconds', durationSeconds);
     // Delay switching duration to avoid NaN
     if (durationSeconds) {
       durationEl.textContent = `${durationMinutes}:${durationSeconds}`;
@@ -111,19 +109,25 @@ function updateProgressBar(e) {
 
     // Calculate Display for current
     const currentMinutes = Math.floor(currentTime / 60);
-    console.log('minutes', currentMinutes);
     let currentSeconds = Math.floor(currentTime % 60);
     if (currentSeconds < 10) {
       currentSeconds = `0${currentSeconds}`;
     }
-    console.log('seconds', currentSeconds);
     currentTimeEl.textContent = `${currentMinutes}:${currentSeconds}`
-
-
   }
+}
+
+// Set Progress Bar
+function setProgressBar(e) {
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  const { duration } = music;
+  music.currentTime = (clickX / width) * duration;
 }
 
 // Event Listeners
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
+music.addEventListener('ended', nextSong);
 music.addEventListener('timeupdate', updateProgressBar);
+progressContainer.addEventListener('click', setProgressBar);
